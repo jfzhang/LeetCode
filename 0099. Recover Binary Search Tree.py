aea73245f4bcc4357f2c8c85ab2384a2fe3549c9
jfzhang <zjf0426@hotmail.com>
2022-03-17 21:10:13 -0700

    add 0099 Recover Binary Search Tree

---
+++ 0099. Recover Binary Search Tree.py
@@ -0,0 +1,58 @@
+"""
+You are given the root of a binary search tree (BST), where the values of exactly two nodes of the tree were swapped by
+mistake. Recover the tree without changing its structure.
+
+Example 1:
+Input: root = [1,3,null,null,2]
+Output: [3,1,null,null,2]
+Explanation: 3 cannot be a left child of 1 because 3 > 1. Swapping 1 and 3 makes the BST valid.
+
+Example 2:
+Input: root = [3,1,4,null,null,2]
+Output: [2,1,4,null,null,3]
+Explanation: 2 cannot be in the right subtree of 3 because 2 < 3. Swapping 2 and 3 makes the BST valid.
+
+Constraints:
+    The number of nodes in the tree is in the range [2, 1000].
+    -231 <= Node.val <= 231 - 1
+
+Follow up: A solution using O(n) space is pretty straight-forward. Could you devise a constant O(1) space solution?
+"""
+
+# Definition for a binary tree node.
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def recoverTree(root: Optional[TreeNode]) -> None:
+    """
+    Do not return anything, modify root in-place instead.
+    """
+    pre = TreeNode(float("-inf"))
+    x, y = None, None
+
+    def inorder(root: Optional[TreeNode]):
+        nonlocal x, y, pre
+
+        if not root:
+            return
+
+        inorder(root.left)
+
+        if root.val <= pre.val and not x:
+            x = pre
+        if root.val <= pre.val and x:
+            y = root
+
+        pre = root
+
+        inorder(root.right)
+
+    inorder(root)
+    x.val, y.val = y.val, x.val
