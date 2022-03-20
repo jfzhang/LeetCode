4aa70e3dd5059908059db0abe7bdccb60b10ed87
jfzhang <zjf0426@hotmail.com>
2022-03-19 22:38:32 -0700

    fine-tune the loop in 0115

--- 0115. Distinct Subsequences.py
+++ 0115. Distinct Subsequences.py
@@ -46,8 +46,8 @@ def numDistinct(s: str, t: str) -> int:
     for i in range(m + 1):  # the "" is a subsequences of any string
         dp[i][0] = 1
 
-    for i in range(1, m + 1):
-        for j in range(1, n + 1):
+    for j in range(1, n + 1):
+        for i in range(j, m + 1):   # s must be longer than t
             if s[i - 1] == t[j - 1]:
                 dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j]
             else:
