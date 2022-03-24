cd41dd5ed659fd437eecf6e8feea0921bc225a6b
jfzhang <zjf0426@hotmail.com>
2022-03-24 16:48:15 -0700

    fine-tuning 216

--- 0126. Word Ladder II.py
+++ 0126. Word Ladder II.py
@@ -34,28 +34,34 @@ from typing import List
 
 
 def findLadders(beginWord: str, endWord: str, wordList: List[str]) -> List[List[str]]:
-    # use BFS
     wordSet = set(wordList)  # use Set to easily find the words
+
+    if endWord not in wordSet:  # edge case
+        return []
+
     word_len = len(beginWord)
-    q = set()
+    q = set()  # used to store the node in each layer when building a tree
     q.add(beginWord)
-    next_to_pre = {}
+    next_map = {}  # a str->Set() hashmap used to store the mapping from one word to others
     found = False
     level = 0
 
-    if endWord not in wordSet:
-        return []
-
     while q:
+        # for save level, there might have multiple solution, so need to loop all elements in this level
         temp_q = q.copy()
         q.clear()
         level += 1
+
+        # remove the visited words, so that the next level would not repeat it to trim the tree
         for curr_word in temp_q:
-            # if the next_word in the WordSet and used,
-            # it could be removed from it, because reusing it has longer path
             if curr_word in wordSet:
                 wordSet.remove(curr_word)
 
+        for curr_word in temp_q:
+            if curr_word not in next_map:
+                next_map[curr_word] = set()
+
+            # loop the possible transform
             for i in range(0, word_len):
                 for j in range(26):
                     if j == ord(curr_word[i]) - ord('a'):
@@ -63,35 +69,40 @@ def findLadders(beginWord: str, endWord: str, wordList: List[str]) -> List[List[
                     next_word = curr_word[0:i] + chr((ord('a') + j)) + curr_word[i + 1:]
                     if next_word not in wordSet:
                         continue
-                    if next_word not in next_to_pre:
-                        next_to_pre[next_word] = set()
-                    next_to_pre[next_word].add(curr_word)
+                    # find a new node
+                    next_map[curr_word].add(next_word)
                     q.add(next_word)
 
                     if not found and next_word == endWord:
                         found = True
-
-        if found:
+        if found:  # if found, don't need to continue building the tree
             break
 
     res = []
 
-    def getPath(begin, end, path, level):
-        if level == 0:
+    # a dfs traversal function to help get the final result
+    def getPath(begin, end, path, seq):
+        # add a path when arriving right level and find the end word
+        if seq == 0:
             if begin == end:
-                res.append(path[::-1])
+                res.append(path[::])
             return
 
-        for x in next_to_pre[end]:
-            getPath(begin, x, path + [x], level-1)
+        for x in next_map[begin]:
+            getPath(x, end, path + [x], seq - 1)
 
     if found:
-        getPath(beginWord, endWord, [endWord], level)
+        getPath(beginWord, endWord, [beginWord], level)
 
     return res
 
 
 # test cases
-#print(findLadders("hit", "cog", ["hot", "dot", "dog", "lot", "log", "cog"]))
-#print(findLadders("red", "tax", ["ted","tex","red","tax","tad","den","rex","pee"]))
-print(findLadders("ta", "if", ["ts","sc","ph","ca","jr","hf","to","if","ha","is","io","cf","ta"]))
+print(findLadders("hit", "cog", ["hot", "dot", "dog", "lot", "log", "cog"]))
+# [['hit', 'hot', 'dot', 'dog', 'cog'], ['hit', 'hot', 'lot', 'log', 'cog']]
+
+print(findLadders("red", "tax", ["ted", "tex", "red", "tax", "tad", "den", "rex", "pee"]))
+# [['red', 'ted', 'tex', 'tax'], ['red', 'ted', 'tad', 'tax'], ['red', 'rex', 'tex', 'tax']]
+
+print(findLadders("ta", "if", ["ts", "sc", "ph", "ca", "jr", "hf", "to", "if", "ha", "is", "io", "cf", "ta"]))
+# [['ta', 'ha', 'hf', 'if'], ['ta', 'to', 'io', 'if'], ['ta', 'ts', 'is', 'if'], ['ta', 'ca', 'cf', 'if']]
